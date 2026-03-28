05a183c328168d5957af13ecd5a683b7521dc911
Dhruv Sabraj <dhruvsabraj@gmail.com>
2026-03-28 19:55:30 +0530

    fixed the framer motion

---
+++ .next/dev/static/chunks/OrbitDrop_src_app_page_tsx_0l325zd._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/OrbitDrop_src_components_CanvasBackground_tsx_0gfppwl._.js","static/chunks/OrbitDrop_src_02.dl3h._.js","static/chunks/0qf__0uafmp0._.js"],
+    source: "dynamic"
+});
